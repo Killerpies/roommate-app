89112b2b4dd154ce6f4559820caa8447284ff528
justin.sanders.20 <justin.sanders.20@cnu.edu>
2022-09-27 12:52:22 -0400

    authentication working

--- src/main.js
+++ src/main.js
@@ -2,5 +2,20 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { createAuth0 } from '@auth0/auth0-vue';
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(store)
+
+app.use(router)
+
+app.use(
+    createAuth0({
+      domain: "dev-3f6063v3.us.auth0.com",
+      client_id: "VjzTdjG6M9W3ZasPstpq1HgPKTK40WtC",
+      redirect_uri: window.location.origin
+    })
+  );
+
+app.mount('#app')
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,48 +1,76 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
+  <div>
+    <button @click="login">Log in</button>
+  </div>
+  <div>
+    <button @click="logout">Log out</button>
+  </div>
+  <div>
+    <pre v-if="isAuthenticated">
+        <code>{{ user }}</code>
+        <code>{{user.id}}</code>
+      </pre>
+  </div>
+  <button @click="printInfo">printInfo</button>
   <h1>HERE IS WHERE LIST IS</h1>
   <div v-for="(item, index) in catnames" v-bind:key="index">
-  <li>{{item.name}}</li>
-</div>
-
+    <li>{{ item.name }}</li>
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import axios from 'axios'
+import HelloWorld from "@/components/HelloWorld.vue";
+import axios from "axios";
+import { useAuth0 } from "@auth0/auth0-vue";
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
   components: {
-    HelloWorld
+    HelloWorld,
+  },
+  setup() {
+    const { loginWithRedirect, logout, user, isAuthenticated } = useAuth0();
+    return {
+      login: () => {
+        loginWithRedirect();
+      },
+      logout: () => {
+        logout({ returnTo: window.location.origin });
+      },
+      user,
+      isAuthenticated,
+    };
   },
   data() {
-    return{
-      catnames: []
-    }
+    return {
+      catnames: [],
+    };
   },
-  mounted(){
-    this.getcat()
-    console.log(window.location.origin)
+  mounted() {
+    this.getcat();
   },
-  methods:{
-    getcat: async function(){
-    try {
-      let url ='/cats'
-      console.log(url)
-        let data =  await axios.get(url)
-        // console.log('data')
-        // console.log(data.data)
-        this.catnames = data.data
-    } catch (error) {
-        console.error(error)
-    }
-    
-    }
-  }
-}
+  methods: {
+    getcat: async function () {
+      try {
+        let url = "/cats";
+
+        let data = await axios.get(url);
+        this.catnames = data.data;
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    printInfo: function () {
+      if (this.isAuthenticated) {
+        // console.log(this.user)
+        // console.log(this.user.user_id)
+      }
+    },
+  },
+};
 </script>
