37be5775217be1bd2bbed98bc08521fe6f78dea3
justin.sanders.20 <justin.sanders.20@cnu.edu>
2022-11-24 20:44:16 -0500

    trying to add sitemap to website

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
+const SitemapPlugin = require("sitemap-webpack-plugin").default;
+// You can write the paths as an array of strings or an array of objects
+const paths = [
+  {
+    path: "/",
+    lastmod: "2022-11-22",
+    priority: 1.0,
+    changefreq: "yearly",
+  },
+];
+
 module.exports = defineConfig({
+  configureWebpack: {
+    plugins: [new SitemapPlugin({ base: "https://roommateapp.org", paths })],
+  },
   transpileDependencies: true,
   devServer: {
-    proxy: 'http://localhost:8080'
-  }
-})
+    proxy: "http://localhost:8080",
+  },
+});
